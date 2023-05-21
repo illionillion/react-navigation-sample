802788d4312c937b8be722c1b33e09550edbd9f4
yuto ichinose <yuto.0526.sano@gmail.com>
2023-05-22 02:57:46 +0900

    編集

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,21 @@
-import { NavigationProp } from "@react-navigation/native";
+import { NavigationProp, RouteProp } from "@react-navigation/native";
 import { View, Text, Button } from "react-native";
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import { Counter } from "../components/Counter";
 
 interface HomeProps {
   navigation: NavigationProp<any, any>;
+  route: RouteProp<any, any>;
 }
-export const HomeScreen:FC<HomeProps> = ({ navigation }) => {
+export const HomeScreen: FC<HomeProps> = ({ navigation, route }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>ホーム画面</Text>
       <Button
         title="ユーザ"
         onPress={() => navigation.navigate("User", { userId: 2 })}
       />
+      <Counter />
     </View>
   );
 };

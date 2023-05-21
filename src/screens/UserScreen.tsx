e4bed1fb01e2a14c355558e97ceb6ebf00dfadeb
yuto ichinose <yuto.0526.sano@gmail.com>
2023-05-22 02:34:38 +0900

    試しに型変えて問題なかった

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,12 +1,13 @@
-import { RouteProp } from "@react-navigation/native";
+import { NavigationProp, RouteProp } from "@react-navigation/native";
 import { View, Text } from "react-native";
 import React from 'react';
 import { Counter } from "../components/Counter";
 
 export interface UserProps {
   route: RouteProp<any, any>;
+  navigation: NavigationProp<any, any>;
 }
-export const UserScreen = ({ route }: UserProps) => {
+export const UserScreen = ({ route, navigation }: UserProps) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>ユーザー画面</Text>
